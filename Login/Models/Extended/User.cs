c89260e4747b5163b0886dc89259811975f4f178
sameeranihathe <sameeranihathe@gmail.com>
2018-05-03 23:35:02 +0530

    data validations

---
+++ Login/Models/Extended/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Login.Models
+{
+    public partial class User
+    {
+    }
+    public class UserMetaData
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage ="Firstname required")]
+        [Display(Name ="First Name")]
+        public string FirstName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "LastName required")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email ID")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email ID is required")]
+        [DataType(DataType.EmailAddress)]
+        public string EmailID { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(ApplyFormatInEditMode =true, DataFormatString ="{0:MM-dd-yyyy}")]
+        public DateTime DateOfBirth { get; set; }
+
+
+        public string Password { get; set; }
+    }
+}
