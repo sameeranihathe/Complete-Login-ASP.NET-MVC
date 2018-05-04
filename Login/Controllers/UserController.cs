a6fab2dc414d7866ace527d51b359f3515417f80
sameeranihathe <sameeranihathe@gmail.com>
2018-05-04 14:15:38 +0530

    send verification email created

--- Login/Controllers/UserController.cs
+++ Login/Controllers/UserController.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -49,11 +51,18 @@ namespace Login.Controllers
             {
                 message = "Invalied request";
             }
-            
 
-            //generate activation code
+            #region save to database
+            using (UserDBEntities db = new UserDBEntities())
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
 
-            //password hashing
+                //send details to user via email
+                sendverificationEmail(user.EmailID, user.ActivationCode.ToString());
+            }
+            #endregion
+
 
             //save data to database
 
@@ -88,6 +97,40 @@ namespace Login.Controllers
             }
         }
 
-       
+        [NonAction]
+        public void sendverificationEmail(string emailID, string activationCode)
+        {
+            var verifyUrl = "/User/VerifyAccount/" + activationCode;
+            var link = Request.Url.AbsoluteUri.Replace(Request.Url.PathAndQuery, verifyUrl);
+
+            var fromEmail = new MailAddress("sameeranihathe@gail.com", "sameera sampath");
+            var toEmail = new MailAddress(emailID);
+            var fromemailPassword = "kanchana143";
+            string subject = "Your account is successfully created";
+
+            string body = "<br/> <br/> Your account is successfully created. Please click the below link to verify the account" +
+                "<br/> <br/> <a href='" + link + "'>" + link + "</a>";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromEmail.Address, fromemailPassword)
+            };
+
+            using (var message = new MailMessage(fromEmail, toEmail)
+            {
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            })
+                smtp.Send(message);
+            
+        }
+
+
     }
 }
