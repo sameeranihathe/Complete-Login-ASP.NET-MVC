3cc5997039935192852e66f4440c9214e82895c1
sameeranihathe <sameeranihathe@gmail.com>
2018-05-04 12:15:17 +0530

    password hashing added

--- Login/Controllers/UserController.cs
+++ Login/Controllers/UserController.cs
@@ -21,6 +21,45 @@ namespace Login.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Registration([Bind(Exclude = "IsEmailVerified, ActivationCode")] User user)
         {
+            bool Status = false;
+            string message = "";
+
+            //model validation
+            if (ModelState.IsValid)
+            {
+                #region //email already exsists
+                var isExist = IsEmailExsist(user.EmailID);
+                if (isExist)
+                {
+                    ModelState.AddModelError("EmailExist", "Email already exists");
+                    return View(user);
+                }
+
+                #region generate activation code
+                user.ActivationCode = Guid.NewGuid();
+                #endregion
+
+                #region password hashing
+                user.Password = Crypto.Hash(user.Password);
+                user.ConfirmPassword = Crypto.Hash(user.ConfirmPassword);
+                #endregion
+                user.IsEmailVerified = false;
+            }
+            else
+            {
+                message = "Invalied request";
+            }
+            
+
+            //generate activation code
+
+            //password hashing
+
+            //save data to database
+
+            //send email to user
+
+
             return View(user);
         }
 
@@ -38,5 +77,17 @@ namespace Login.Controllers
 
 
         //LOgout
+
+        [NonAction]
+        public bool IsEmailExsist(string emailID)
+        {
+            using (UserDBEntities db = new UserDBEntities())
+            {
+                var v = db.Users.Where(a => a.EmailID == emailID).FirstOrDefault();
+                return v != null;
+            }
+        }
+
+       
     }
 }
---
+++ Login/Crypto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Login
+{
+    public class Crypto
+    {
+    }
+}
